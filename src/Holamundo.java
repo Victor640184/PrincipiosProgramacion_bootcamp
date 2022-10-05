aa8b62a05abe7e6b6946f96b843541404c3ac7fd
Victor640184 <victorperez640184@gmail.com>
2022-10-04 21:07:52 -0500

    Como prueba de funcionamiento cambie la frase en el SOUP

--- src/Holamundo.java
+++ src/Holamundo.java
@@ -1,7 +1,7 @@
 public class Holamundo {
     public static void main(String[] args){
 
-        System.out.println("Hola que tal!!");
+        System.out.println("Hola que tal como van!!");
     }
 
 }
